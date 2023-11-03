e085d93e626638c4e240a8d3b59191e44944ff8e
@Faimasoft786 <FatimaSoft786@gmail.com>
2023-11-03 20:26:48 +0500

    Edit the codde

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -5,13 +5,10 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const randomString = require("randomstring");
 const { body, validationResult } = require('express-validator');
-<<<<<<< HEAD
-
-=======
 const fetchuser = require('../middleware/FetchUser');
 
 const JWT_SECRET = 'Shahzaibisagoodb$oy';
->>>>>>> 07e4c2e4ec880b023db5863d16a3e6a86986f706
+
 
 //endpoint to create user
 router.post("/createuser",[
@@ -59,12 +56,12 @@ router.post('/login', [
     return res.status(400).json({ errors: errors.array() });
   }
   const { username, password } = req.body;
+  console.log(req.body)
   try {
     let user = await User.findOne({ username });
     if (!user) {
       return res.status(400).json({ error: "Please try to login with correct credentials" });
     }
-
     const passwordCompare = await bcrypt.compare(password, user.password);
     if (!passwordCompare) {
       return res.status(400).json({ success, error: "Please try to login with correct credentials" });
@@ -77,8 +74,9 @@ router.post('/login', [
     let userId = user._id
     isAdmin = user.isAdmin
    // console.log(data.user.id);
-    const authtoken = jwt.sign(userId, JWT_SECRET);
-    res.json({user,isAdmin, authtoken })
+  //  const authtoken = jwt.sign(userId, JWT_SECRET);
+   // res.json({user,isAdmin, authtoken })
+   res.status(200).send({isAdmin,id: userId})
 
   } catch (error) {
     res.status(500).send({error: error.message});
@@ -117,55 +115,55 @@ router.post('/login', [
 // });
 
 // //endpoint to access all the users except the user who's is currently logged in!
-// router.get("/users/:userId", fetchuser, (req, res) => {
-//   const loggedInUserId = req.params.userId;
+router.get("/users/:userId", (req, res) => {
+  const loggedInUserId = req.params.userId;
 
-//   User.find({ _id: { $ne: loggedInUserId } }).select("-password")
-//     .then((users) => {
-//       res.status(200).json({users: users});
-//     })
-//     .catch((err) => {
-//       console.log("Error retrieving users", err);
-//       res.status(500).json({ error: "Error retrieving users" });
-//     });
-// });
+  User.find({ _id: { $ne: loggedInUserId } }).select("-password")
+    .then((users) => {
+      res.status(200).json({users: users});
+    })
+    .catch((err) => {
+      console.log("Error retrieving users", err);
+      res.status(500).json({ error: "Error retrieving users" });
+    });
+});
 
-// //enpoint Get loggedin User Details using: POST "/api/auth/getuser". Login required
-// router.get('/getuser/:id', fetchuser,  async (req, res) => {
-//   try {
-//     const user = await User.findById(req.params.id).select("-password")
-//     res.send({data: user})
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).send("Internal Server Error");
-//   }
-// })
+//enpoint Get loggedin User Details using: POST "/api/auth/getuser". Login required
+router.get('/getuser/:id',  async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password")
+    res.send({data: user})
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 
 // //endpoint to change username
-// router.post("/change-username/:id",
-// body('username', 'Enter a valid username').isLength({ min: 3 }),
-// fetchuser,async(req,res)=>{
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ error: errors.array() });
-//   }
-//   try {
-//     const data = await User.findOne({_id: req.params.id });
-//     console.log(data);
-//       const userData = await User.findByIdAndUpdate(
-//         { _id: data._id },
-//         { $set: { username: req.body.username} },
-//         { new: true }
-//       );
-//       res
-//         .status(200)
-//         .send({
-//           successMessage: 'Your username has been updated'
-//         });
-//   } catch (error) {
-//     res.status(500).send({ error: error.message })
-//   }
-// });
+router.post("/change-username/:id",
+body('username', 'Enter a valid username').isLength({ min: 3 }),
+fetchuser,async(req,res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: errors.array() });
+  }
+  try {
+    const data = await User.findOne({_id: req.params.id });
+    console.log(data);
+      const userData = await User.findByIdAndUpdate(
+        { _id: data._id },
+        { $set: { username: req.body.username} },
+        { new: true }
+      );
+      res
+        .status(200)
+        .send({
+          successMessage: 'Your username has been updated'
+        });
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+});
 // //endpoint to change password
 // router.post('/change-password',[
 //   body('username', 'Enter a valid username').isLength({ min: 3 }),
@@ -203,7 +201,7 @@ router.post('/login', [
 //         message: 'Your password has been updated'
 //       });
     
-<<<<<<< HEAD
+
 //   } catch (error) {
 //     res.status(400).send({error: error.message})
 //   }
@@ -222,11 +220,11 @@ router.post('/login', [
 //   res.status(400).send({error: error.message})
 // }
 // });
-=======
-  } catch (error) {
-    res.status(400).send({error: error.message})
-  }
-});
+
+//   } catch (error) {
+//     res.status(400).send({error: error.message})
+//   }
+// });
 //endpoint to delete the user
 router.delete("/delete-user/:id",fetchuser,async(req,res)=>{
 try {
@@ -241,6 +239,6 @@ try {
   res.status(400).send({error: error.message})
 }
 });
->>>>>>> 07e4c2e4ec880b023db5863d16a3e6a86986f706
+
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 const connectMongo = require('./db');
 const express = require('express');
 var cors  = require('cors');
+var admin = require("firebase-admin");
+var serviceAccount = require("./middleware/service.json");
 require('dotenv').config();
 connectMongo();
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+ app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use('/api/auth', require('./Routes/authRoute'))
 app.use('/api/user',require("./Routes/calendarRoute"));
 app.use('/api/user/message',require('./Routes/messageRoute'));
 
+app.use(function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+ // projectId: 'rnnotification-447e3',
+});
+
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(process.env.GOOGLE_CREDENTIALS)
+// });
+
+app.post("/send", async(req, res) =>{
+  const receivedToken = req.body.fcmToken;
+  
+  const message = {
+    notification: {
+      title: "Notif",
+      body: 'This is a Test Notification'
+    },
+    token: receivedToken,
+  };
+
+
+await  admin.messaging()
+    .send(message)
+    .then((response) => {
+      res.status(200).json({
+        message: "Successfully sent message",
+        token: receivedToken,
+      });
+      console.log("Successfully sent message:", response);
+    })
+    .catch((error) => {
+      res.status(400);
+      res.send(error);
+      console.log("Error sending message:", error);
+    });
+});
 app.listen(port, ()=>{
     console.log("listening perfect");
   });
